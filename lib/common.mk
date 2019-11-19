7a5cb65c2562261184514ca6cee84585c06e25c8
Toke Høiland-Jørgensen <toke@redhat.com>
2019-11-19 20:49:01 +0100

    Man pages should be .8 (for root-only system commands)
    Also, make sure we put them in man8/ beneath MANDIR
    
    Signed-off-by: Toke Høiland-Jørgensen <toke@redhat.com>

--- lib/common.mk
+++ lib/common.mk
@@ -37,7 +37,7 @@ CFLAGS += -I$(HEADER_DIR) -I$(LIB_DIR)/util
 BPF_CFLAGS += -I$(HEADER_DIR)
 
 BPF_HEADERS := $(wildcard $(HEADER_DIR)/bpf/*.h)
-MAN_FILES := $(wildcard ${USER_TARGETS:=.1})
+MAN_FILES := $(wildcard ${USER_TARGETS:=.8})
 
 all: $(USER_TARGETS) $(XDP_OBJ)
 
@@ -53,8 +53,8 @@ install:
 	install -m 0755 -d $(DESTDIR)$(BPF_OBJECT_DIR)
 	install -m 0755 $(USER_TARGETS) $(DESTDIR)$(SBINDIR)
 	install -m 0644 $(XDP_OBJ) $(DESTDIR)$(BPF_OBJECT_DIR)
-	$(if $(MAN_FILES),install -m 0755 -d $(DESTDIR)$(MANDIR))
-	$(if $(MAN_FILES),install -m 0644 $(MAN_FILES) $(DESTDIR)$(MANDIR))
+	$(if $(MAN_FILES),install -m 0755 -d $(DESTDIR)$(MANDIR)/man8)
+	$(if $(MAN_FILES),install -m 0644 $(MAN_FILES) $(DESTDIR)$(MANDIR)/man8)
 
 $(OBJECT_LIBBPF):
 	$(Q)$(MAKE) -C $(LIB_DIR) libbpf
