229790708447264c3af3a88a1254d406a2d7dacf
Štěpán Horáček <shoracek@redhat.com>
2020-09-24 17:02:03 +0200

    testing: Move tests into separate directories
    Signed-off-by: Štěpán Horáček <shoracek@redhat.com>

--- lib/testing/run_tests.sh
+++ lib/testing/run_tests.sh
@@ -7,7 +7,12 @@ TEST_RUNNER="$TEST_PROG_DIR/test_runner.sh"
 RET=0
 
 echo "Running all tests from $TESTS_DIR"
-for f in "$TESTS_DIR"/*.sh; do
+for f in "$TESTS_DIR"/*/test-*.sh; do
+    if [[ ! -f "$f" ]]; then
+        echo "No tests found!"
+        exit 1
+    fi
+
     "$TEST_RUNNER" "$f" || RET=1
 done
 
