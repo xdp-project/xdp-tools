xdp-project/xdp-tools

commit debc9d5a4d7ec0e47287ef603649db57e89d20d6

xdp: add libxdp_clean_references
This change adds an API to libxdp to clean up dead references that
remain in bpffs after a net_device goes away, and the program is
detached. This helper can figure out if the pinned directory is dead,
and if so, clean it up.

Passing an ifindex of 0 has special semantics, in that it will traverse
through the entire bpffs tree looking for dead links, performing a
complete sweep.

This will be used to implement the xdp-loader clean command in a
subsequent patch.

Signed-off-by: Kumar Kartikeya Dwivedi <memxor@gmail.com>
[ toke: Fixup log message on clean ]
Signed-off-by: Toke Høiland-Jørgensen <toke@redhat.com>

lib/libxdp/libxdp.c
@@ -16,7 +16,9 @@
 #include <sys/mman.h>
 #include <sys/file.h>
 #include <sys/vfs.h>
+#include <sys/types.h>
 #include <fcntl.h>
+#include <inttypes.h>
 #include <dirent.h>
 
 #include <linux/err.h> /* ERR_PTR */
@@ -2569,4 +2571,127 @@ struct xdp_program *xdp_multiprog__main_prog(const struct xdp_multiprog *mp)
 bool xdp_multiprog__is_legacy(const struct xdp_multiprog *mp)
 {
 	return !!(mp && mp->is_legacy);
+}
+
+static __u32 xdp_get_ifindex_prog_id(int ifindex)
+{
+	struct xdp_link_info xinfo = {};
+	__u32 prog_id = 0;
+
+	if (!bpf_get_link_xdp_info(ifindex, &xinfo, sizeof(xinfo), 0)) {
+		switch (xinfo.attach_mode) {
+		case XDP_ATTACHED_SKB:
+			prog_id = xinfo.skb_prog_id;
+			break;
+		case XDP_ATTACHED_DRV:
+			prog_id = xinfo.drv_prog_id;
+			break;
+		case XDP_ATTACHED_MULTI:
+			prog_id = xinfo.drv_prog_id ?: xinfo.skb_prog_id;
+			break;
+		case XDP_ATTACHED_NONE:
+		case XDP_ATTACHED_HW:
+		default:;
+		}
+	}
+	return prog_id;
+}
+
+static int remove_pin_dir(const char *subdir)
+{
+	char prog_path[PATH_MAX], pin_path[PATH_MAX];
+	int err;
+	DIR *d;
+
+	const char *dir = get_bpffs_dir();
+	if (IS_ERR(dir))
+		return PTR_ERR(dir);
+
+	err = try_snprintf(pin_path, sizeof(pin_path), "%s/%s", dir, subdir);
+	if (err)
+		return err;
+
+	d = opendir(pin_path);
+	if (!d) {
+		err = -errno;
+		pr_warn("Failed to open pin directory: %s\n", strerror(-err));
+		return err;
+	}
+
+	for (struct dirent *dent = readdir(d); dent; dent = readdir(d)) {
+		/* skip . and .. */
+		if (dent->d_type == DT_DIR)
+			continue;
+
+		err = try_snprintf(prog_path, sizeof(prog_path), "%s/%s",
+				   pin_path, dent->d_name);
+		if (err)
+			goto err;
+
+		err = unlink(prog_path);
+		if (err) {
+			err = -errno;
+			pr_warn("Couldn't unlink file %s/%s: %s\n", subdir,
+				dent->d_name, strerror(-err));
+			goto err;
+		}
+	}
+	err = rmdir(pin_path);
+	if (err) {
+		err = -errno;
+		pr_warn("Failed to remove pin directory %s: %s\n", pin_path,
+			strerror(-err));
+	}
+err:
+	closedir(d);
+	return err;
+}
+
+int libxdp_clean_references(int ifindex)
+{
+	int err, lock_fd, path_ifindex;
+	__u32 dir_prog_id, prog_id;
+	DIR *d;
+
+	const char *dir = get_bpffs_dir();
+	if (IS_ERR(dir))
+		return PTR_ERR(dir);
+
+	lock_fd = xdp_lock_acquire();
+	if (lock_fd < 0)
+		return lock_fd;
+
+	d = opendir(dir);
+	if (!d) {
+		err = -errno;
+		pr_debug("Failed to open bpffs directory: %s\n",
+			 strerror(-err));
+		goto err;
+	}
+
+	for (struct dirent *dent = readdir(d); dent; dent = readdir(d)) {
+		if (dent->d_type != DT_DIR)
+			continue;
+
+		if (sscanf(dent->d_name, "dispatch-%d-%"PRIu32"",
+			   &path_ifindex, &dir_prog_id) != 2)
+			continue;
+
+		/* If ifindex is set, skip this dir if it doesn't match */
+		if (ifindex && path_ifindex != ifindex)
+			continue;
+
+		prog_id = xdp_get_ifindex_prog_id(path_ifindex);
+		if (!prog_id || prog_id != dir_prog_id) {
+			pr_info("Prog id %"PRIu32" no longer attached on ifindex %d, removing pin directory %s\n",
+				dir_prog_id, path_ifindex, dent->d_name);
+			err = remove_pin_dir(dent->d_name);
+			if (err)
+				goto err;
+		}
+	}
+err:
+	closedir(d);
+	xdp_lock_release(lock_fd);
+	return err;
 }

headers/xdp/libxdp.h
@@ -41,6 +41,7 @@ struct xdp_multiprog;
 
 long libxdp_get_error(const void *ptr);
 int libxdp_strerror(int err, char *buf, size_t size);
+int libxdp_clean_references(int ifindex);
 
 
 struct xdp_program *xdp_program__from_bpf_obj(struct bpf_object *obj,

lib/libxdp/libxdp.map
@@ -38,5 +38,6 @@ LIBXDP_1.0.0 {
 		xdp_program__tag;
 };
 
-LIBXDP_1.1.0 {
+LIBXDP_1.2.0 {
+		libxdp_clean_references;
 } LIBXDP_1.0.0;

commit 2a4e3d45b94da5ddc60f6606d84b212bfde5f135

libxdp: Initialise err variable in libxdp_clean_references()
If nothing was removed the err variable would never be initialised, leading
to a return of an uninitialised value.

Fixes: debc9d5a4d7e ("xdp: add libxdp_clean_references")
Signed-off-by: Toke Høiland-Jørgensen <toke@redhat.com>

lib/libxdp/libxdp.c
@@ -2698,7 +2698,7 @@ err:
 
 int libxdp_clean_references(int ifindex)
 {
-	int err, lock_fd, path_ifindex;
+	int err = 0, lock_fd, path_ifindex;
 	__u32 dir_prog_id, prog_id;
 	DIR *d;
 