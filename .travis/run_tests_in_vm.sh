8a686157c7c1b57dda6c7491a51e49ec1d489163
Toke Høiland-Jørgensen <toke@redhat.com>
2021-05-28 14:39:43 +0200

    .travis: try using qemu64 virtual CPU with accel=tcg
    Let's see if this works at all before trying to get anything running in
    Github actions...
    
    Signed-off-by: Toke Høiland-Jørgensen <toke@redhat.com>

--- .travis/run_tests_in_vm.sh
+++ .travis/run_tests_in_vm.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo virtme-run --kdir kernel --script-exec .travis/run_tests.sh --pwd --rw --mods=auto --qemu-opts -m 512M
+sudo virtme-run --kdir kernel --script-exec .travis/run_tests.sh --pwd --rw --mods=auto --qemu-opts -m 512M -machine accel=tcg -cpu qemu64
 exit "$(cat RESULT)"
