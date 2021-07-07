de787cf00cf9cbb3e8b17d3cfce371c0881c92e8
Toke Høiland-Jørgensen <toke@redhat.com>
2021-07-07 16:44:50 +0200

    headers: include icmp.h and if.h from kernel sources
    The kernel if.h may end up including system headers which tends to break
    BPF builds. So include our own version of icmp.h and if.h, truncating the
    latter to only include the size definitions needed by the former.
    
    Signed-off-by: Toke Høiland-Jørgensen <toke@redhat.com>

---
+++ headers/linux/if.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0+ WITH Linux-syscall-note */
+/* Truncated header from the kernel sources that just defines the name sizes
+ * below; used by icmp.h */
+#ifndef _LINUX_IF_H
+#define _LINUX_IF_H
+
+#define	IFNAMSIZ	16
+#define	IFALIASZ	256
+#define	ALTIFNAMSIZ	128
+
+#endif /* _LINUX_IF_H */
