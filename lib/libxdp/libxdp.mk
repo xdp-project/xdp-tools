76d22acb45eeb634d1a01c5e4e1b1b7bde54594e
Toke Høiland-Jørgensen <toke@redhat.com>
2021-07-07 16:01:49 +0200

    lib/libxdp: Automatically populate LIBXDP_VERSION from libxdp.map
    Instead of having to manually update the library version when tagging a new
    release, just populate the variable by parsing it from the libxdp.map file.
    Also remove the instructions in the packaging README about updating this
    version.
    
    Signed-off-by: Toke Høiland-Jørgensen <toke@redhat.com>

--- lib/libxdp/libxdp.mk
+++ lib/libxdp/libxdp.mk
@@ -1,2 +1,3 @@
-LIBXDP_MAJOR_VERSION := 1
-LIBXDP_VERSION := 1.1.0
+LIBXDP_VERSION := $(shell sed -ne "/LIBXDP_[0-9\.]\+ {/ {s/LIBXDP_\([0-9\.]\+\) {/\1/;p}" $(LIB_DIR)/libxdp/libxdp.map | tail -n 1)
+LIBXDP_MAJOR_VERSION := $(shell echo $(LIBXDP_VERSION) | sed 's/\..*//')
+
