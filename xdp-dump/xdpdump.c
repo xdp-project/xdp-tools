xdp-project/xdp-tools

commit cbc47d53b275a26945b9cc9a5e4a731541038564

xdp-dump: add support for multiple buffer wakeup
Fixes: #28, #42

Signed-off-by: Eelco Chaudron <echaudro@redhat.com>

xdp-dump/xdpdump.c
@@ -56,6 +56,7 @@ static const struct dumpopt {
 	bool                  hex_dump;
 	struct iface          iface;
 	uint32_t              snaplen;
+	uint32_t              perf_wakeup;
 	char                 *pcap_file;
 	char                 *program_names;
 	unsigned int          rx_capture;
@@ -68,6 +69,7 @@ struct dumpopt cfg_dumpopt;
 
 static struct prog_option xdpdump_options[] = {
 	DEFINE_OPTION("rx-capture", OPT_FLAGS, struct dumpopt, rx_capture,
+		      .short_opt = 1,
 		      .metavar = "<mode>",
 		      .typearg = rx_capture_flags,
 		      .help = "Capture point for the rx direction"),
@@ -79,6 +81,11 @@ static struct prog_option xdpdump_options[] = {
 		      .short_opt = 'i',
 		      .metavar = "<ifname>",
 		      .help = "Name of interface to capture on"),
+#ifdef HAVE_LIBBPF_PERF_BUFFER__CONSUME
+	DEFINE_OPTION("perf-wakeup", OPT_U32, struct dumpopt, perf_wakeup,
+		      .metavar = "<events>",
+		      .help = "Wake up xdpdump every <events> packets"),
+#endif
 	DEFINE_OPTION("program-names", OPT_STRING, struct dumpopt,
 		      program_names,
 		      .short_opt = 'p',
@@ -735,6 +742,34 @@ rlimit_loop:
 	perf_ctx.pcap_dumper = pcap_dumper;
 	perf_ctx.epoch_delta = get_epoch_to_uptime_delta();
 
+	/* Determine the perf wakeup_events value to use */
+#ifdef HAVE_LIBBPF_PERF_BUFFER__CONSUME
+	if (cfg->perf_wakeup) {
+		perf_attr.wakeup_events = cfg->perf_wakeup;
+	} else {
+		if (cfg->pcap_file &&
+		    cfg->pcap_file[0] == '-' && cfg->pcap_file[1] == 0) {
+			/* If we pipe trough stdio we do not want to buffer
+			 * any packets in the perf ring.
+			 */
+			perf_attr.wakeup_events = 1;
+		} else {
+			/*
+			 * If no specific wakeup value is specified assume
+			 * an average packet size of 2K we would like to
+			 * fill without loosing any packets.
+			 */
+			uint32_t events = PERF_MMAP_PAGE_COUNT * getpagesize() /
+				libbpf_num_possible_cpus() / 2048;
+
+			if (events > 0)
+				perf_attr.wakeup_events = min(PERF_MAX_WAKEUP_EVENTS,
+							      events);
+		}
+	}
+#endif
+	pr_debug("perf-wakeup value uses is %u\n", perf_attr.wakeup_events);
+
 	/* Setup perf ring buffers */
 	perf_opts.attr = &perf_attr;
 	perf_opts.event_cb = handle_perf_event;
@@ -751,6 +786,9 @@ rlimit_loop:
 			goto error_exit;
 		}
 	}
+#ifdef HAVE_LIBBPF_PERF_BUFFER__CONSUME
+	perf_buffer__consume(perf_buf);
+#endif
 
 	fprintf(stderr, "\n%"PRIu64" packets captured\n",
 		perf_ctx.captured_packets);

configure
@@ -102,6 +102,25 @@ check_zlib()
     fi
 }
 
+check_perf_consume()
+{
+    cat >$TMPDIR/libbpftest.c <<EOF
+#include <bpf/libbpf.h>
+int main(int argc, char **argv) {
+    perf_buffer__consume(NULL);
+    return 0;
+}
+EOF
+    libbpf_err=$($CC -o $TMPDIR/libbpftest $TMPDIR/libbpftest.c $LIBBPF_CFLAGS $LIBBPF_LDLIBS 2>&1)
+    if [ "$?" -eq "0" ]; then
+        echo "HAVE_LIBBPF_PERF_BUFFER__CONSUME:=y" >>"$CONFIG"
+        echo "yes"
+    else
+        echo "HAVE_LIBBPF_PERF_BUFFER__CONSUME:=n" >>"$CONFIG"
+        echo "no"
+    fi
+}
+
 check_libbpf()
 {
     local libbpf_err
@@ -136,7 +155,10 @@ EOF
 	    echo 'CFLAGS += ' $LIBBPF_CFLAGS >> $CONFIG
 	    echo 'LDLIBS += ' $LIBBPF_LDLIBS >>$CONFIG
             echo 'OBJECT_LIBBPF = ' >>$CONFIG
-	    echo system
+            echo system
+
+            echo -n "perf_buffer__consume support: "
+            check_perf_consume
             return 0
         fi
     else
@@ -169,6 +191,11 @@ EOF
 
     echo -n "zlib support: "
     check_zlib || exit 1
+
+    # For the build submodule library we know it does support this API, so we
+    # hard code it. Also due to the fact it's hard to build a test app as
+    # libbpf.a has not been build at configure time.
+    echo "HAVE_LIBBPF_PERF_BUFFER__CONSUME:=y" >>"$CONFIG"
 }
 
 quiet_config()

lib/defines.mk
@@ -24,6 +24,10 @@ ifneq ($(PRODUCTION),1)
 DEFINES += -DDEBUG
 endif
 
+ifeq ($(HAVE_LIBBPF_PERF_BUFFER__CONSUME),y)
+DEFINES +=-DHAVE_LIBBPF_PERF_BUFFER__CONSUME
+endif
+
 CFLAGS += -Werror $(DEFINES)
 BPF_CFLAGS += $(DEFINES)
 

xdp-dump/README.org
@@ -36,6 +36,7 @@ Options:
      --rx-capture <mode>    Capture point for the rx direction (valid values: entry,exit)
  -D, --list-interfaces      Print the list of available interfaces
  -i, --interface <ifname>   Name of interface to capture on
+     --perf-wakeup <events>  Wake up xdpdump every <events> packets
  -p, --program-names <prog>  Specific program to attach to
  -s, --snapshot-length <snaplen>  Minimum bytes of packet to capture
  -w, --write <file>         Write raw packets to pcap file
@@ -60,6 +61,11 @@ Display a list of available interfaces and any XDP program loaded
 ** -i, --interface <ifname>
 Listen on interface =ifname=. Note that if no XDP program is loaded on the
 interface it will use libpcap's live capture mode to capture the packets.
+** --perf-wakeup <events>
+Let the Kernel wake up =xdpdump= once for every =<events>= being posted in the
+perf ring buffer. The higher the number the less the impact is on the actual
+XDP program. The default value is 0, which automatically calculates the
+value based on the available CPUs/buffers. Use -v to see the actual used value.
 ** -p, --program-names <prog>
 The Linux API does not provide the full name of the attached eBPF entry function
 if it's longer than 15 characters. xdpdump will try to guess the correct

xdp-dump/test-xdpdump.sh
@@ -52,6 +52,7 @@ Options:
      --rx-capture <mode>    Capture point for the rx direction (valid values: entry,exit)
  -D, --list-interfaces      Print the list of available interfaces
  -i, --interface <ifname>   Name of interface to capture on
+     --perf-wakeup <events>  Wake up xdpdump every <events> packets
  -p, --program-names <prog>  Specific program to attach to
  -s, --snapshot-length <snaplen>  Minimum bytes of packet to capture
  -w, --write <file>         Write raw packets to pcap file

xdp-dump/xdpdump.h
@@ -9,7 +9,8 @@
 /******************************************************************************
  * General definitions
  ******************************************************************************/
-#define PERF_MMAP_PAGE_COUNT	16
+#define PERF_MAX_WAKEUP_EVENTS   64
+#define PERF_MMAP_PAGE_COUNT	256
 #define MAX_CPUS		256
 
 /******************************************************************************

commit 3c40bbe273459da98d1023aac592357170bbe1f2

xdpdump: only buffer in perf ring when using pcap_file
It is confusing that xdpdump buffers packets (via perf_attr.wakeup_events)
when printing to stdout, as described in #130.

This fixes #130 by only buffering packet in the perf ring
when cmdline option --write pcap_file have been specified.

Fixes: cbc47d53b275 ("xdp-dump: add support for multiple buffer wakeup")
Signed-off-by: Jesper Dangaard Brouer <brouer@redhat.com>

xdp-dump/xdpdump.c
@@ -1747,11 +1747,8 @@ static bool capture_on_interface(struct dumpopt *cfg)
 
 	/* Determine the perf wakeup_events value to use */
 #ifdef HAVE_LIBBPF_PERF_BUFFER__CONSUME
-	if (cfg->perf_wakeup) {
-		perf_attr.wakeup_events = cfg->perf_wakeup;
-	} else {
-		if (cfg->pcap_file &&
-		    cfg->pcap_file[0] == '-' && cfg->pcap_file[1] == 0) {
+	if (cfg->pcap_file) {
+		if (cfg->pcap_file[0] == '-' && cfg->pcap_file[1] == 0) {
 			/* If we pipe trough stdio we do not want to buffer
 			 * any packets in the perf ring.
 			 */
@@ -1769,7 +1766,13 @@ static bool capture_on_interface(struct dumpopt *cfg)
 				perf_attr.wakeup_events = min(PERF_MAX_WAKEUP_EVENTS,
 							      events);
 		}
+	} else {
+		/* Only buffer in perf ring when using pcap_file */
+		perf_attr.wakeup_events = 1;
 	}
+	/* Cmdline option --perf-wakeup can override buffering levels */
+	if (cfg->perf_wakeup)
+		perf_attr.wakeup_events = cfg->perf_wakeup;
 #endif
 	pr_debug("perf-wakeup value uses is %u\n", perf_attr.wakeup_events);
 