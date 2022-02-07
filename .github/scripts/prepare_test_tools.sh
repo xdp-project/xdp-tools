a508d9f0db14875dd6fcd1741f3a0e3e41a6f800
Eelco Chaudron <echaudro@redhat.com>
2022-02-07 15:31:53 +0100

    github: remove bpftools as a dependency for the test runs
    Signed-off-by: Eelco Chaudron <echaudro@redhat.com>

--- .github/scripts/prepare_test_tools.sh
+++ .github/scripts/prepare_test_tools.sh
@@ -2,11 +2,6 @@
 
 set -e
 
-echo ::group::Install bpftool
-git clone --depth=1 https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git bpftool
-sudo make install -C bpftool/tools/bpf/bpftool/ prefix=/usr
-echo ::endgroup::
-
 
 echo ::group::Install xdp-test-harness
 sudo python3 -m pip install xdp_test_harness
