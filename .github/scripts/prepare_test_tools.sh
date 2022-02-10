b1f72360213a6f87a0c783206026231b4207a26e
Toke Høiland-Jørgensen <toke@redhat.com>
2022-02-10 22:42:52 +0100

    github/workflows: Run the selftests on multiple architectures
    We're already running the tests in QEMU, so it only takes a bit of surgery
    to enable them to run on all the architectures we can pull the Fedora
    kernels from.
    
    Signed-off-by: Toke Høiland-Jørgensen <toke@redhat.com>

--- .github/scripts/prepare_test_tools.sh
+++ .github/scripts/prepare_test_tools.sh
@@ -17,16 +17,3 @@ echo ::group::Install virtme
 git clone https://github.com/amluto/virtme
 sudo python3 -m pip install ./virtme
 echo ::endgroup::
-
-
-if [[ $KERNEL_VERSION == "LATEST" ]]; then
-    echo ::group::Install pahole
-    # In the repo there is only version 1.15 and we need newer.
-    git clone https://git.kernel.org/pub/scm/devel/pahole/pahole.git
-    mkdir pahole/build
-    cd pahole/build
-    cmake -D__LIB=lib ..
-    sudo make install
-    sudo ldconfig /usr/local/lib
-    echo ::endgroup::
-fi
