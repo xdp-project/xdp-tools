xdp-project/xdp-tools

commit 8ed7e26968445a676fac7b3ced97699390cd6245

libxdp: export inline helpers as symbols in libxdp
This helps people writing language bindings to not have to rewrite C
wrappers for inline functions in the headers. We force inline the
definition from the header for C and C++ consumers, but also export a
symbol in the library for others. This keeps the performance advantages.

Also see
https://lore.kernel.org/bpf/CAJ8uoz0QqR97qEYYK=VVCE9A=V=k2tKnH6wNM48jeak2RAmL0A@mail.gmail.com/
for some context.

extern inline is used as it's slightly better since it warns when an
inline definition is missing.

Signed-off-by: Kumar Kartikeya Dwivedi <memxor@gmail.com>

headers/xdp/xsk.h
@@ -45,7 +45,7 @@ DEFINE_XSK_RING(xsk_ring_cons);
 struct xsk_umem;
 struct xsk_socket;
 
-static inline __u64 *xsk_ring_prod__fill_addr(struct xsk_ring_prod *fill,
+XDP_ALWAYS_INLINE __u64 *xsk_ring_prod__fill_addr(struct xsk_ring_prod *fill,
 					      __u32 idx)
 {
 	__u64 *addrs = (__u64 *)fill->ring;
@@ -53,7 +53,7 @@ static inline __u64 *xsk_ring_prod__fill_addr(struct xsk_ring_prod *fill,
 	return &addrs[idx & fill->mask];
 }
 
-static inline const __u64 *
+XDP_ALWAYS_INLINE const __u64 *
 xsk_ring_cons__comp_addr(const struct xsk_ring_cons *comp, __u32 idx)
 {
 	const __u64 *addrs = (const __u64 *)comp->ring;
@@ -61,7 +61,7 @@ xsk_ring_cons__comp_addr(const struct xsk_ring_cons *comp, __u32 idx)
 	return &addrs[idx & comp->mask];
 }
 
-static inline struct xdp_desc *xsk_ring_prod__tx_desc(struct xsk_ring_prod *tx,
+XDP_ALWAYS_INLINE struct xdp_desc *xsk_ring_prod__tx_desc(struct xsk_ring_prod *tx,
 						      __u32 idx)
 {
 	struct xdp_desc *descs = (struct xdp_desc *)tx->ring;
@@ -69,7 +69,7 @@ static inline struct xdp_desc *xsk_ring_prod__tx_desc(struct xsk_ring_prod *tx,
 	return &descs[idx & tx->mask];
 }
 
-static inline const struct xdp_desc *
+XDP_ALWAYS_INLINE const struct xdp_desc *
 xsk_ring_cons__rx_desc(const struct xsk_ring_cons *rx, __u32 idx)
 {
 	const struct xdp_desc *descs = (const struct xdp_desc *)rx->ring;
@@ -77,12 +77,12 @@ xsk_ring_cons__rx_desc(const struct xsk_ring_cons *rx, __u32 idx)
 	return &descs[idx & rx->mask];
 }
 
-static inline int xsk_ring_prod__needs_wakeup(const struct xsk_ring_prod *r)
+XDP_ALWAYS_INLINE int xsk_ring_prod__needs_wakeup(const struct xsk_ring_prod *r)
 {
 	return *r->flags & XDP_RING_NEED_WAKEUP;
 }
 
-static inline __u32 xsk_prod_nb_free(struct xsk_ring_prod *r, __u32 nb)
+XDP_ALWAYS_INLINE __u32 xsk_prod_nb_free(struct xsk_ring_prod *r, __u32 nb)
 {
 	__u32 free_entries = r->cached_cons - r->cached_prod;
 
@@ -102,7 +102,7 @@ static inline __u32 xsk_prod_nb_free(struct xsk_ring_prod *r, __u32 nb)
 	return r->cached_cons - r->cached_prod;
 }
 
-static inline __u32 xsk_cons_nb_avail(struct xsk_ring_cons *r, __u32 nb)
+XDP_ALWAYS_INLINE __u32 xsk_cons_nb_avail(struct xsk_ring_cons *r, __u32 nb)
 {
 	__u32 entries = r->cached_prod - r->cached_cons;
 
@@ -114,7 +114,7 @@ static inline __u32 xsk_cons_nb_avail(struct xsk_ring_cons *r, __u32 nb)
 	return (entries > nb) ? nb : entries;
 }
 
-static inline __u32 xsk_ring_prod__reserve(struct xsk_ring_prod *prod, __u32 nb, __u32 *idx)
+XDP_ALWAYS_INLINE __u32 xsk_ring_prod__reserve(struct xsk_ring_prod *prod, __u32 nb, __u32 *idx)
 {
 	if (xsk_prod_nb_free(prod, nb) < nb)
 		return 0;
@@ -125,7 +125,7 @@ static inline __u32 xsk_ring_prod__reserve(struct xsk_ring_prod *prod, __u32 nb,
 	return nb;
 }
 
-static inline void xsk_ring_prod__submit(struct xsk_ring_prod *prod, __u32 nb)
+XDP_ALWAYS_INLINE void xsk_ring_prod__submit(struct xsk_ring_prod *prod, __u32 nb)
 {
 	/* Make sure everything has been written to the ring before indicating
 	 * this to the kernel by writing the producer pointer.
@@ -133,7 +133,7 @@ static inline void xsk_ring_prod__submit(struct xsk_ring_prod *prod, __u32 nb)
 	libbpf_smp_store_release(prod->producer, *prod->producer + nb);
 }
 
-static inline __u32 xsk_ring_cons__peek(struct xsk_ring_cons *cons, __u32 nb, __u32 *idx)
+XDP_ALWAYS_INLINE __u32 xsk_ring_cons__peek(struct xsk_ring_cons *cons, __u32 nb, __u32 *idx)
 {
 	__u32 entries = xsk_cons_nb_avail(cons, nb);
 
@@ -145,12 +145,12 @@ static inline __u32 xsk_ring_cons__peek(struct xsk_ring_cons *cons, __u32 nb, __
 	return entries;
 }
 
-static inline void xsk_ring_cons__cancel(struct xsk_ring_cons *cons, __u32 nb)
+XDP_ALWAYS_INLINE void xsk_ring_cons__cancel(struct xsk_ring_cons *cons, __u32 nb)
 {
 	cons->cached_cons -= nb;
 }
 
-static inline void xsk_ring_cons__release(struct xsk_ring_cons *cons, __u32 nb)
+XDP_ALWAYS_INLINE void xsk_ring_cons__release(struct xsk_ring_cons *cons, __u32 nb)
 {
 	/* Make sure data has been read before indicating we are done
 	 * with the entries by updating the consumer pointer.
@@ -158,22 +158,22 @@ static inline void xsk_ring_cons__release(struct xsk_ring_cons *cons, __u32 nb)
 	libbpf_smp_store_release(cons->consumer, *cons->consumer + nb);
 }
 
-static inline void *xsk_umem__get_data(void *umem_area, __u64 addr)
+XDP_ALWAYS_INLINE void *xsk_umem__get_data(void *umem_area, __u64 addr)
 {
 	return &((char *)umem_area)[addr];
 }
 
-static inline __u64 xsk_umem__extract_addr(__u64 addr)
+XDP_ALWAYS_INLINE __u64 xsk_umem__extract_addr(__u64 addr)
 {
 	return addr & XSK_UNALIGNED_BUF_ADDR_MASK;
 }
 
-static inline __u64 xsk_umem__extract_offset(__u64 addr)
+XDP_ALWAYS_INLINE __u64 xsk_umem__extract_offset(__u64 addr)
 {
 	return addr >> XSK_UNALIGNED_BUF_OFFSET_SHIFT;
 }
 
-static inline __u64 xsk_umem__add_offset_to_addr(__u64 addr)
+XDP_ALWAYS_INLINE __u64 xsk_umem__add_offset_to_addr(__u64 addr)
 {
 	return xsk_umem__extract_addr(addr) + xsk_umem__extract_offset(addr);
 }

headers/xdp/libxdp_util.h
@@ -70,6 +70,8 @@ extern "C" {
 	})
 #endif
 
+#define XDP_ALWAYS_INLINE inline __attribute__((__always_inline__))
+
 #ifdef __cplusplus
 } /* extern "C" */
 #endif

lib/libxdp/libxdp.map
@@ -50,4 +50,20 @@ LIBXDP_1.2.0 {
 		xsk_umem__create;
 		xsk_umem__delete;
 		xsk_umem__fd;
+		xsk_cons_nb_avail;
+		xsk_prod_nb_free;
+		xsk_ring_cons__cancel;
+		xsk_ring_cons__comp_addr;
+		xsk_ring_cons__peek;
+		xsk_ring_cons__release;
+		xsk_ring_cons__rx_desc;
+		xsk_ring_prod__fill_addr;
+		xsk_ring_prod__needs_wakeup;
+		xsk_ring_prod__reserve;
+		xsk_ring_prod__submit;
+		xsk_ring_prod__tx_desc;
+		xsk_umem__add_offset_to_addr;
+		xsk_umem__extract_addr;
+		xsk_umem__extract_offset;
+		xsk_umem__get_data;
 } LIBXDP_1.0.0;

lib/libxdp/xsk.c
@@ -101,6 +101,30 @@ struct xdp_mmap_offsets_v1 {
 	struct xdp_ring_offset_v1 cr;
 };
 
+/* Export all inline helpers as symbols for use by language bindings. */
+extern inline __u64 *xsk_ring_prod__fill_addr(struct xsk_ring_prod *fill,
+					      __u32 idx);
+extern inline const __u64 *
+xsk_ring_cons__comp_addr(const struct xsk_ring_cons *comp, __u32 idx);
+extern inline struct xdp_desc *xsk_ring_prod__tx_desc(struct xsk_ring_prod *tx,
+						      __u32 idx);
+extern inline const struct xdp_desc *
+xsk_ring_cons__rx_desc(const struct xsk_ring_cons *rx, __u32 idx);
+extern inline int xsk_ring_prod__needs_wakeup(const struct xsk_ring_prod *r);
+extern inline __u32 xsk_prod_nb_free(struct xsk_ring_prod *r, __u32 nb);
+extern inline __u32 xsk_cons_nb_avail(struct xsk_ring_cons *r, __u32 nb);
+extern inline __u32 xsk_ring_prod__reserve(struct xsk_ring_prod *prod, __u32 nb,
+					   __u32 *idx);
+extern inline void xsk_ring_prod__submit(struct xsk_ring_prod *prod, __u32 nb);
+extern inline __u32 xsk_ring_cons__peek(struct xsk_ring_cons *cons, __u32 nb,
+					__u32 *idx);
+extern inline void xsk_ring_cons__cancel(struct xsk_ring_cons *cons, __u32 nb);
+extern inline void xsk_ring_cons__release(struct xsk_ring_cons *cons, __u32 nb);
+extern inline void *xsk_umem__get_data(void *umem_area, __u64 addr);
+extern inline __u64 xsk_umem__extract_addr(__u64 addr);
+extern inline __u64 xsk_umem__extract_offset(__u64 addr);
+extern inline __u64 xsk_umem__add_offset_to_addr(__u64 addr);
+
 int xsk_umem__fd(const struct xsk_umem *umem)
 {
 	return umem ? umem->fd : -EINVAL;

commit 83549d3a2e662e3a4238c6f539048e1fdf7fa895

xdp/xsk.h: Fix compilation with -std=gnu89
Commit 8ed7e2696844 ("libxdp: export inline helpers as symbols in
libxdp") used C99 inline semantics to rely on inline without extern not
emitting a symbol, and extern inline declaration doing that instead. On
C89, these C99 inline semantics are not followed.

Instead of requiring users to migrate away from C89, we can still retain
compatibility by marking XDP_ALWAYS_INLINE as static inline, while
ensuring libxdp continues to expose these symbols.

Closes: #173
Fixes: 8ed7e2696844 ("libxdp: export inline helpers as symbols in libxdp")
Signed-off-by: Kumar Kartikeya Dwivedi <memxor@gmail.com>

headers/xdp/xsk.h
@@ -21,7 +21,11 @@
 extern "C" {
 #endif
 
+#ifdef __GNUC_STDC_INLINE__
 #define XDP_ALWAYS_INLINE inline __attribute__((__always_inline__))
+#elif __GNUC_GNU_INLINE__
+#define XDP_ALWAYS_INLINE static inline __attribute__((__always_inline__))
+#endif
 
 /* Do not access these members directly. Use the functions below. */
 #define DEFINE_XSK_RING(name) \