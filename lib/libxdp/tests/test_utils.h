7431a94d3f93eafa6508d1933fe7d18fd102e2a9
Toke Høiland-Jørgensen <toke@redhat.com>
2023-01-20 17:20:00 +0100

    libxdp: Add selftest for frags support
    Add a selftest program for libxdp frags support, testing the different
    permutations of loading a program with and without frags support.
    
    Signed-off-by: Toke Høiland-Jørgensen <toke@redhat.com>

--- lib/libxdp/tests/test_utils.h
+++ lib/libxdp/tests/test_utils.h
@@ -4,6 +4,7 @@
 #define __TEST_UTILS_H
 
 #include <bpf/libbpf.h>
+#include <xdp/libxdp.h>
 
 #define __unused __attribute__((unused))
 
@@ -14,9 +15,35 @@ static int libbpf_silent_func(__unused enum libbpf_print_level level,
 	return 0;
 }
 
-static void silence_libbpf_logging(void)
+static inline void silence_libbpf_logging(void)
 {
 	libbpf_set_print(libbpf_silent_func);
+}
+
+static int libxdp_silent_func(__unused enum libxdp_print_level level,
+			      __unused const char *format,
+			      __unused va_list args)
+{
+	return 0;
+}
+
+static int libxdp_verbose_func(__unused enum libxdp_print_level level,
+			       __unused const char *format,
+			       __unused va_list args)
+{
+	fprintf(stderr, "  ");
+	vfprintf(stderr, format, args);
+	return 0;
+}
+
+static inline void silence_libxdp_logging(void)
+{
+	libxdp_set_print(libxdp_silent_func);
+}
+
+static inline void verbose_libxdp_logging(void)
+{
+	libxdp_set_print(libxdp_verbose_func);
 }
 
 #endif
