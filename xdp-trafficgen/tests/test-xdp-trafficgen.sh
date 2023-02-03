95f02d0bd935075f9e5e1408a3ece98172759928
Toke Høiland-Jørgensen <toke@redhat.com>
2023-02-03 16:54:05 +0100

    xdp-trafficgen: Add selftests
    Add simple selftests for xdp-trafficgen; just checking that running in each
    mode is possible.
    
    Signed-off-by: Toke Høiland-Jørgensen <toke@redhat.com>

---
+++ xdp-trafficgen/tests/test-xdp-trafficgen.sh
@@ -0,0 +1,36 @@
+XDP_LOADER=${XDP_LOADER:-./xdp-loader}
+XDP_TRAFFICGEN=${XDP_BENCH:-./xdp-trafficgen}
+ALL_TESTS="test_udp test_tcp"
+
+PIDS=""
+
+skip_if_missing_kernel_support()
+{
+    $XDP_TRAFFICGEN probe || exit $SKIPPED_TEST
+}
+
+test_udp()
+{
+    skip_if_missing_kernel_support
+    export XDP_SAMPLE_IMMEDIATE_EXIT=1
+
+    check_run $XDP_TRAFFICGEN udp $NS -n 1
+}
+
+test_tcp()
+{
+    skip_if_missing_kernel_support
+    export XDP_SAMPLE_IMMEDIATE_EXIT=1
+
+    PID=$(start_background_ns_devnull "nc -6 -l 10000")
+    $XDP_TRAFFICGEN tcp -i $NS $INSIDE_IP6 -n 1
+    res=$?
+    stop_background $PID
+    return $res
+}
+
+cleanup_tests()
+{
+    $XDP_LOADER unload $NS --all >/dev/null 2>&1
+    $XDP_LOADER clean >/dev/null 2>&1
+}
