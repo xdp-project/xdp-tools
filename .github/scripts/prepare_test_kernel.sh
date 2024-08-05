a301ba835c3c1c4411a83873372f2cc2c95f06e5
Toke Høiland-Jørgensen <toke@redhat.com>
2024-08-05 12:34:59 +0200

    .github/workflows: Add more recent kernel versions to text matrix
    Add 6.6 and 6.10 kernels to the test matrix. Change the test definition format a
    bit to make it less verbose on the YAML side, making it easier to add new kernel
    versions in the future.
    
    Signed-off-by: Toke Høiland-Jørgensen <toke@redhat.com>

--- .github/scripts/prepare_test_kernel.sh
+++ .github/scripts/prepare_test_kernel.sh
@@ -2,25 +2,23 @@
 
 set -e
 
-if [[ "$KERNEL_VERSION" == "LATEST" ]]; then
-    git clone --depth 1 https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git kernel
-    cd kernel
-    cp "$GITHUB_WORKSPACE"/.github/scripts/.config .config
-    make -j $(nproc) olddefconfig all
-else
-    KERNEL_VERSION_COMPLETE="$KERNEL_VERSION"-"$KERNEL_PATCH_VERSION".x86_64
-    PACKAGES_URL=https://kojipkgs.fedoraproject.org/packages/kernel/
-    PACKAGES_URL+="$KERNEL_VERSION"/"$KERNEL_PATCH_VERSION"/x86_64
+IFS=- read KERNEL_UPSTREAM_VERSION KERNEL_PATCH_VERSION <<< $KERNEL_VERSION
+KERNEL_VERSION_COMPLETE="$KERNEL_VERSION".x86_64
+PACKAGES_URL=https://kojipkgs.fedoraproject.org/packages/kernel/
+PACKAGES_URL+="$KERNEL_UPSTREAM_VERSION"/"$KERNEL_PATCH_VERSION"/x86_64
 
-    for package in core modules modules-extra devel; do
-        wget -nv "$PACKAGES_URL"/kernel-"$package"-"$KERNEL_VERSION_COMPLETE".rpm
-        rpm2cpio kernel-"$package"-"$KERNEL_VERSION_COMPLETE".rpm | cpio -di
-    done
-    find lib -name "*.xz" -exec xz -d {} \;
+for package in core modules modules-core modules-extra devel; do
+    # modules-core package only exists for newer kernel versions, so continue if
+    # download fails
+    wget -nv "$PACKAGES_URL"/kernel-"$package"-"$KERNEL_VERSION_COMPLETE".rpm || continue
+    rpm2cpio kernel-"$package"-"$KERNEL_VERSION_COMPLETE".rpm | cpio -di
+done
+find lib -name "*.xz" -exec xz -d {} \;
 
-    mv lib/modules/"$KERNEL_VERSION_COMPLETE" kernel
-    mkdir -p kernel/arch/x86/boot
-    cp kernel/vmlinuz kernel/arch/x86/boot/bzImage
-    cp kernel/config kernel/.config
-    rsync -a usr/src/kernels/"$KERNEL_VERSION_COMPLETE"/ kernel/
-fi
+mv lib/modules/"$KERNEL_VERSION_COMPLETE" kernel
+mkdir -p kernel/arch/x86/boot
+cp kernel/vmlinuz kernel/arch/x86/boot/bzImage
+cp kernel/config kernel/.config
+rsync -a usr/src/kernels/"$KERNEL_VERSION_COMPLETE"/ kernel/
+
+find kernel
