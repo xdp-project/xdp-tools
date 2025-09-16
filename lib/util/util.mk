9ffe911757b300c3ca18cb6543bd793641d382a5
Toke Høiland-Jørgensen <toke@redhat.com>
2025-09-16 13:38:28 +0200

    lib/util: Import xdpsock code from bpf_examples
    This includes the xdpsock code from the bpf_examples repository into
    lib/util. The code is included as-is, with only a couple of small
    changes to make the code compile (fixing "mixed-sign compare" compiler
    warnings). The following commits will refactor the code into a library
    to be re-used by xdp-bench and xdp-trafficgen.
    
    Signed-off-by: Toke Høiland-Jørgensen <toke@redhat.com>

--- lib/util/util.mk
+++ lib/util/util.mk
@@ -5,7 +5,7 @@ UTIL_OBJS := params.o logging.o util.o stats.o xpcapng.o
 UTIL_BPF_OBJS :=
 
 ifneq ($(BPFTOOL),)
-UTIL_OBJS += xdp_sample.o
-UTIL_BPF_OBJS += xdp_sample.bpf.o xdp_load_bytes.bpf.o
+UTIL_OBJS += xdp_sample.o xdpsock.o
+UTIL_BPF_OBJS += xdp_sample.bpf.o xdp_load_bytes.bpf.o xdpsock.bpf.o
 endif
 
---
+++ lib/util/xdpsock.bpf.c
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: GPL-2.0
+#include <linux/bpf.h>
+#include <bpf/bpf_helpers.h>
+
+/* This XDP program is only needed for multi-buffer and XDP_SHARED_UMEM modes.
+ * If you do not use these modes, libxdp can supply an XDP program for you.
+ */
+
+#define MAX_SOCKS 4
+struct {
+	__uint(type, BPF_MAP_TYPE_XSKMAP);
+	__uint(max_entries, MAX_SOCKS);
+	__uint(key_size, sizeof(int));
+	__uint(value_size, sizeof(int));
+} xsks_map SEC(".maps");
+
+int num_socks = 0;
+static unsigned int rr;
+
+SEC("xdp_sock") int xdp_sock_prog(struct xdp_md *ctx)
+{
+	rr = (rr + 1) & (num_socks - 1);
+	return bpf_redirect_map(&xsks_map, rr, XDP_DROP);
+}
