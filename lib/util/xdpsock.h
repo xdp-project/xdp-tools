50ff189a16a0452c010be017c812b1bc3e5269ca
Toke Høiland-Jørgensen <toke@redhat.com>
2025-09-16 18:55:37 +0200

    lib/xdpsock: Restructure setup code to use context object
    Restructure the setup code of xdpsock to get rid of the main() function
    and instead introduce a new xsk_ctx__create() function that creates the
    previously introduced context object and sets everything up.
    
    Signed-off-by: Toke Høiland-Jørgensen <toke@redhat.com>

--- lib/util/xdpsock.h
+++ lib/util/xdpsock.h
@@ -96,6 +96,7 @@ struct xsk_ctx {
 	unsigned long tx_cycle_ns;
 
 	unsigned long start_time;
+	unsigned long duration;
 	bool benchmark_done;
 
 	__u32 irq_no;
@@ -117,5 +118,12 @@ struct xsk_ctx {
 };
 
 int xsk_validate_opts(const struct xsk_opts *opt);
+struct xsk_ctx *xsk_ctx__create(const struct xsk_opts *opt,
+				enum xsk_benchmark_type bench);
+void xsk_ctx__destroy(struct xsk_ctx *ctx);
+void xsk_rx_drop_all(struct xsk_ctx *ctx);
+int xsk_tx_only_all(struct xsk_ctx *ctx);
+void xsk_l2fwd_all(struct xsk_ctx *ctx);
+int xsk_start_poller_thread(struct xsk_ctx *ctx, pthread_t *pt);
 
 #endif /* XDPSOCK_H */
