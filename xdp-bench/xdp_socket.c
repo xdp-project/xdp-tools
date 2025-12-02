a2a9790e763a7e93b104d5a4625676df391f14f8
Toke Høiland-Jørgensen <toke@redhat.com>
2025-12-02 17:03:18 +0100

    xdp-bench: Implement xsk-drop and xdp-tx actions
    Wire up the xdpsock library code to implement the xsk-drop and xsk-tx
    commands in xdp-bench.
    
    Signed-off-by: Toke Høiland-Jørgensen <toke@redhat.com>

--- xdp-bench/xdp_socket.c
+++ xdp-bench/xdp_socket.c
@@ -4,6 +4,7 @@
 #include <bpf/bpf.h>
 #include <stdbool.h>
 #include <linux/bpf.h>
+#include <pthread.h>
 #include <bpf/libbpf.h>
 #include <sys/resource.h>
 #include <linux/if_link.h>
@@ -24,9 +25,51 @@ const struct xsk_opts defaults_xsk = {
 };
 
 
-int do_xsk(const void *cfg, __unused const char *pin_root_path)
+static int do_xsk(const struct xsk_opts *opt,
+		  enum xsk_benchmark_type bench)
+{
+	struct xsk_ctx *ctx;
+	pthread_t pt;
+	int ret;
+
+	ret = xsk_validate_opts(opt);
+	if (ret)
+		return ret;
+
+	ctx = xsk_ctx__create(opt, bench);
+	ret = libxdp_get_error(ctx);
+	if (ret)
+		return ret;
+
+	if (!opt->quiet) {
+		ret = xsk_start_poller_thread(ctx, &pt);
+		if (ret)
+			goto out;
+	}
+
+	if (bench == XSK_BENCH_RXDROP)
+		xsk_rx_drop_all(ctx);
+	else if (bench == XSK_BENCH_L2FWD)
+		xsk_l2fwd_all(ctx);
+
+	if (!opt->quiet)
+		pthread_join(pt, NULL);
+
+out:
+	xsk_ctx__destroy(ctx);
+	return ret;
+}
+
+int do_xsk_drop(const void *cfg, __unused const char *pin_root_path)
 {
 	const struct xsk_opts *opt = cfg;
 
-	return xsk_validate_opts(opt);
+	return do_xsk(opt, XSK_BENCH_RXDROP);
+}
+
+int do_xsk_tx(const void *cfg, __unused const char *pin_root_path)
+{
+	const struct xsk_opts *opt = cfg;
+
+	return do_xsk(opt, XSK_BENCH_L2FWD);
 }
