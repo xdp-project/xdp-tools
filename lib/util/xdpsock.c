xdp-project/xdp-tools

commit be036c8a5b36234c403a559ba8e09076d569eccc

util/xdpsock: Ensure progress on completing frames
The xdpsock code has a couple of places where it loops trying to reserve
enough spaces in the ring buffer for returning frames. This can loop
forever in some cases when the test is exiting.

To prevent this, don't loop forever waiting for new slots to become
available in the producer ring. Instead, fall back to reserving slots
one at a time while processing the received batch, and abort processing
if even this fails. This ensures we return to the outer loop in a fixed
time, avoiding the infinite hang.

Signed-off-by: Toke Høiland-Jørgensen <toke@redhat.com>

lib/util/xdpsock.c
@@ -1140,29 +1140,37 @@ static inline int complete_tx_l2fwd(struct xsk_socket_info *xsk,
 	/* re-add completed Tx buffers */
 	rcvd = xsk_ring_cons__peek(&umem->cq, ndescs, &idx_cq);
 	if (rcvd > 0) {
-		unsigned int i;
-		int ret;
+		unsigned int i, slots;
 
-		ret = xsk_ring_prod__reserve(&umem->fq, rcvd, &idx_fq);
-		while (ret != (int)rcvd) {
-			if (ret < 0)
-				return ret;
+		slots = xsk_ring_prod__reserve(&umem->fq, rcvd, &idx_fq);
+		if (slots < rcvd) {
 			if (busy_poll || xsk_ring_prod__needs_wakeup(&umem->fq)) {
 				xsk->app_stats.fill_fail_polls++;
 				if (recvfrom(xsk_socket__fd(xsk->xsk), NULL, 0,
 					     MSG_DONTWAIT, NULL, NULL) < 0)
 					return -errno;
 			}
-			ret = xsk_ring_prod__reserve(&umem->fq, rcvd, &idx_fq);
+			slots = xsk_ring_prod__reserve(&umem->fq, rcvd, &idx_fq);
 		}
 
-		for (i = 0; i < rcvd; i++)
+		for (i = 0; i < rcvd; i++) {
+			if (!slots) {
+				/* couldn't reserve the full number of slots above, try
+				 * reserving one at a time while processing, aborting if
+				 * this fails
+				 */
+				slots = xsk_ring_prod__reserve(&umem->fq, 1, &idx_fq);
+				if (!slots)
+					break;
+			}
+			slots--;
 			*xsk_ring_prod__fill_addr(&umem->fq, idx_fq++) =
 				*xsk_ring_cons__comp_addr(&umem->cq, idx_cq++);
+		}
 
-		xsk_ring_prod__submit(&xsk->umem->fq, rcvd);
-		xsk_ring_cons__release(&xsk->umem->cq, rcvd);
-		xsk->outstanding_tx -= rcvd;
+		xsk_ring_prod__submit(&xsk->umem->fq, i);
+		xsk_ring_cons__release(&xsk->umem->cq, i);
+		xsk->outstanding_tx -= i;
 	}
 	return 0;
 }
@@ -1190,9 +1198,8 @@ static inline void complete_tx_only(struct xsk_socket_info *xsk,
 
 static int rx_drop(struct xsk_socket_info *xsk, __u32 batch_size, bool busy_poll)
 {
-	unsigned int rcvd, i, eop_cnt = 0;
+	unsigned int rcvd, slots, i, eop_cnt = 0;
 	__u32 idx_rx = 0, idx_fq = 0;
-	int ret;
 
 	rcvd = xsk_ring_cons__peek(&xsk->rx, batch_size, &idx_rx);
 	if (!rcvd) {
@@ -1205,37 +1212,45 @@ static int rx_drop(struct xsk_socket_info *xsk, __u32 batch_size, bool busy_poll
 		return 0;
 	}
 
-	ret = xsk_ring_prod__reserve(&xsk->umem->fq, rcvd, &idx_fq);
-	while ((unsigned int)ret != rcvd) {
-		if (ret < 0)
-			return ret;
+	slots = xsk_ring_prod__reserve(&xsk->umem->fq, rcvd, &idx_fq);
+	if (slots < rcvd) {
 		if (busy_poll || xsk_ring_prod__needs_wakeup(&xsk->umem->fq)) {
 			xsk->app_stats.fill_fail_polls++;
 			if (recvfrom(xsk_socket__fd(xsk->xsk), NULL, 0,
 				     MSG_DONTWAIT, NULL, NULL) < 0)
 				return -errno;
 		}
-		ret = xsk_ring_prod__reserve(&xsk->umem->fq, rcvd, &idx_fq);
+		slots = xsk_ring_prod__reserve(&xsk->umem->fq, rcvd, &idx_fq);
 	}
 
 	for (i = 0; i < rcvd; i++) {
-		const struct xdp_desc *desc = xsk_ring_cons__rx_desc(&xsk->rx, idx_rx++);
+		const struct xdp_desc *desc = xsk_ring_cons__rx_desc(&xsk->rx, idx_rx + i);
 		__u64 addr = desc->addr;
 		__u32 len = desc->len;
 		__u64 orig = xsk_umem__extract_addr(addr);
-		eop_cnt += IS_EOP_DESC(desc->options);
 
 		addr = xsk_umem__add_offset_to_addr(addr);
 		char *pkt = xsk_umem__get_data(xsk->umem->buffer, addr);
 
+		if (!slots) {
+			/* couldn't reserve the full number of slots above, try
+			 * reserving one at a time while processing, aborting if
+			 * this fails
+			 */
+			slots = xsk_ring_prod__reserve(&xsk->umem->fq, 1, &idx_fq);
+			if (!slots)
+				break;
+		}
+		slots--;
 		hex_dump(pkt, len, addr);
 		*xsk_ring_prod__fill_addr(&xsk->umem->fq, idx_fq++) = orig;
+		eop_cnt += IS_EOP_DESC(desc->options);
 	}
 
-	xsk_ring_prod__submit(&xsk->umem->fq, rcvd);
-	xsk_ring_cons__release(&xsk->rx, rcvd);
+	xsk_ring_prod__submit(&xsk->umem->fq, i);
+	xsk_ring_cons__release(&xsk->rx, i);
 	xsk->ring_stats.rx_npkts += eop_cnt;
-	xsk->ring_stats.rx_frags += rcvd;
+	xsk->ring_stats.rx_frags += i;
 
 	return 0;
 }

commit 194770bb806b4d5fbb789364030ff6734e266dbb

util/xdpsock: Fix another instance of infinite looping
The previous fix for infinite looping in xdpsock missed one instance of
the same looping pattern. Apply the same gradual reservation pattern
here as the previous fix.

Fixes: be036c8a5b36 ("util/xdpsock: Ensure progress on completing frames")
Signed-off-by: Toke Høiland-Jørgensen <toke@redhat.com>

lib/util/xdpsock.c
@@ -1463,7 +1463,7 @@ static void *xsk_tx_only_all(void *arg)
 static int l2fwd(struct xsk_ctx *ctx, struct xsk_socket_info *xsk)
 {
 	__u32 idx_rx = 0, idx_tx = 0, frags_done = 0;
-	unsigned int rcvd, i, eop_cnt = 0;
+	unsigned int rcvd, i, eop_cnt = 0, slots;
 	static __u32 nb_frags;
 	int ret;
 
@@ -1480,10 +1480,8 @@ static int l2fwd(struct xsk_ctx *ctx, struct xsk_socket_info *xsk)
 		return 0;
 	}
 
-	ret = xsk_ring_prod__reserve(&xsk->tx, rcvd, &idx_tx);
-	while ((unsigned int)ret != rcvd) {
-		if (ret < 0)
-			return ret;
+	slots = xsk_ring_prod__reserve(&xsk->tx, rcvd, &idx_tx);
+	if (slots < rcvd) {
 		complete_tx_l2fwd(xsk, ctx->opt.batch_size, ctx->opt.busy_poll);
 		if (ctx->opt.busy_poll || xsk_ring_prod__needs_wakeup(&xsk->tx)) {
 			xsk->app_stats.tx_wakeup_sendtos++;
@@ -1491,11 +1489,11 @@ static int l2fwd(struct xsk_ctx *ctx, struct xsk_socket_info *xsk)
 			if (ret)
 				return ret;
 		}
-		ret = xsk_ring_prod__reserve(&xsk->tx, rcvd, &idx_tx);
+		slots = xsk_ring_prod__reserve(&xsk->tx, rcvd, &idx_tx);
 	}
 
 	for (i = 0; i < rcvd; i++) {
-		const struct xdp_desc *desc = xsk_ring_cons__rx_desc(&xsk->rx, idx_rx++);
+		const struct xdp_desc *desc = xsk_ring_cons__rx_desc(&xsk->rx, idx_rx + i);
 		bool eop = IS_EOP_DESC(desc->options);
 		__u64 addr = desc->addr;
 		__u32 len = desc->len;
@@ -1503,6 +1501,17 @@ static int l2fwd(struct xsk_ctx *ctx, struct xsk_socket_info *xsk)
 
 		addr = xsk_umem__add_offset_to_addr(addr);
 		char *pkt = xsk_umem__get_data(xsk->umem->buffer, addr);
+
+		if (!slots) {
+			/* couldn't reserve the full number of slots above, try
+			 * reserving one at a time while processing, aborting if
+			 * this fails
+			 */
+			slots = xsk_ring_prod__reserve(&xsk->tx, 1, &idx_tx);
+			if (!slots)
+				break;
+		}
+		slots--;
 
 		if (!nb_frags++)
 			swap_mac_addresses(pkt);
@@ -1527,8 +1536,8 @@ static int l2fwd(struct xsk_ctx *ctx, struct xsk_socket_info *xsk)
 
 	xsk->ring_stats.rx_npkts += eop_cnt;
 	xsk->ring_stats.tx_npkts += eop_cnt;
-	xsk->ring_stats.rx_frags += rcvd;
-	xsk->ring_stats.tx_frags += rcvd;
+	xsk->ring_stats.rx_frags += i;
+	xsk->ring_stats.tx_frags += i;
 	xsk->outstanding_tx += frags_done;
 
 	return 0;